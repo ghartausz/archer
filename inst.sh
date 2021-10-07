7085b102624b1489f0d783399b9201f53e96ff6d
ghartausz <92097535+ghartausz@users.noreply.github.com>
2021-10-07 23:54:34 +0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -10,25 +10,19 @@ Y="\e[33m" #YELLOW
 b="\e[1m" #bold
 
 echo
-echo "genfstab -U /mnt >> /mnt/etc/fstab"
-genfstab -U /mnt >> /mnt/etc/fstab
-echo
-echo -e "Switching fromt the live ${Y}iso/arch install${E} to the recently installed ${C}Arch Linux${E}"
-echo "arch-chroot /mnt"
-arch-chroot /mnt
-echo
 echo -e "${Y}Time zones${E}:"
 ls /usr/share/zoneinfo
 echo "Type your ${Y}Time zone${E} from the above list"
-read timezone
+read -r timezone
 ls /usr/share/zoneinfo/$timezone
 echo "Type your ${Y}City${E} from the list"
-read city
+read -r city
 echo "ln -sf /usr/share/zoneinfo/$timezone/$city /etc/localtime"
 ln -sf /usr/share/zoneinfo/$timezone/$city /etc/localtime
 echo "hwclock --systohc"
 hwclock --systohc
-echo "Uncommenting the en_US.UTF-8 UTF-8 line"
+echo 
+echo -e "${B}Uncommenting${E}the en_US.UTF-8 UTF-8 line"
 sed -i '19,$s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 echo "Generating locale"
 locale-gen
@@ -36,6 +30,7 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo -e "Type in your ${R}hostname${E}:"
 read -r hostname
 echo "$hostname" >> /etc/hostname
+echo
 echo -e "Creating entrties like ${C}127.0.0..${E} to the /etc/hosts file"
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
@@ -45,4 +40,5 @@ echo -e "${P}Initramfs${E}"
 mkinitcpio -P
 echo -e "Type in your ${R}root password${R}:"
 passwd
+echo -e "Next step...bootloader"
 
