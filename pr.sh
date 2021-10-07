3220a012d0194e412b7a27eab0f29cb3dda52ac5
ghartausz <92097535+ghartausz@users.noreply.github.com>
2021-10-08 00:44:21 +0300

    Create pr.sh

---
+++ pr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+C="\e[36m" #CYAN
+E="\e[0m" #ENDCOLOR
+G="\e[32m" #GREEN
+LB="\e[1;34m" #LIGHT BLUE
+B="\e[34m" #BLUE
+P="\e[35m" #PURPLE
+Y="\e[33m" #YELLOW
+#b="\e[1m" #bold
+
+echo
+echo -e "${Y}Time zones${E}:"
+ls /usr/share/zoneinfo
+echo "Type your ${Y}Time zone${E} from the above list"
+read -r timezone
+ls /usr/share/zoneinfo/$timezone
+echo "Type your ${Y}City${E} from the list"
+read -r city
+echo "ln -sf /usr/share/zoneinfo/$timezone/$city /etc/localtime"
+ln -sf /usr/share/zoneinfo/$timezone/$city /etc/localtime
+echo "hwclock --systohc"
+hwclock --systohc
