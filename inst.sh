818f176aebd8ba99cec94d56736475f68f418154
ghartausz <92097535+ghartausz@users.noreply.github.com>
2021-10-08 01:03:31 +0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -12,10 +12,10 @@ Y="\e[33m" #YELLOW
 echo
 echo -e "${Y}Time${E} ${Y}zones${E}:"
 ls /usr/share/zoneinfo
-echo "Type your ${Y}Time${E} ${Y}zone${E} from the above list"
+echo -e "Type your ${Y}Time${E} ${Y}zone${E} from the above list"
 read -r timezone
 ls /usr/share/zoneinfo/"$timezone"
-echo "Type your ${Y}City${E} from the list"
+echo -e "Type your ${Y}City${E} from the list"
 read -r city
 echo "ln -sf /usr/share/zoneinfo/"$timezone"/"$city" /etc/localtime"
 ln -sf /usr/share/zoneinfo/"$timezone"/"$city" /etc/localtime
