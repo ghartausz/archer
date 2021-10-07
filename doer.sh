9f17c5357d512847c9787e39e326f3bb122170aa
ghartausz <92097535+ghartausz@users.noreply.github.com>
2021-10-07 17:15:33 +0300

    Update doer.sh

--- doer.sh
+++ doer.sh
@@ -2,9 +2,25 @@
 
 C="\e[36m" #CYAN
 E="\e[0m" #ENDCOLOR
+G="\e[31m" #GREEN
+LB="\e[1;34m" #LIGHT BLUE
+B=="\e[34m" #BLUE
 
 echo -e "Multiple setps wait ahead, choose ${C}wisely${E}:"
 echo " "
-echo "Looking for the ${C} EFI ${E} mode"
+echo "Checking for${C} EFI ${E} mode"
 ls /sys/firmware/efi/efivars
+echo " "
+echo -e "${G}Network{E} interfaces: "
+ip link
+echo -e "Ping${G}Google{E} : "
+ping -c 3 google.com
+echo -e "Updating system ${LB}clock{E}: "
+timedatectl set-ntp true
+echo -e "${B}Identifying${E} devices and paritions: "
+lsblk
+echo -e "Choose the disk/device for ${B}partitioning ${E}: "
+read thesda
+echo -e "${B}Partitioning${E} with gdisk utility: "
+gdisk /dev/thesda
 printf "END"
