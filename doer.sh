a8a933269b5a2c2354eb844376ce962463338807
ghartausz <92097535+ghartausz@users.noreply.github.com>
2021-10-07 17:02:08 +0300

    Update doer.sh

--- doer.sh
+++ doer.sh
@@ -5,6 +5,6 @@ E="\e[0m" #ENDCOLOR
 
 echo -e "Multiple setps wait ahead, choose ${C}wisely${E}:"
 echo " "
-echo "Looking for the ${C} EFI mode${E}"
+echo "Looking for the ${C} EFI ${E} mode"
 ls /sys/firmware/efi/efivars
 printf "END"
