c5b40b8ea3268328da0749b1d758ef23bcc0bcaa
ghartausz <92097535+ghartausz@users.noreply.github.com>
2022-10-08 21:55:57 +0300

    Update doer.sh

--- doer.sh
+++ doer.sh
@@ -216,19 +216,20 @@ pacman -Syu
 echo
 echo -e "${R}DONE, at least with the basic ones${E}"
 echo
-read -p "Do you want to install the 3D stuff ${G}now${E} [press ${G}Y${E}] or ${R}later${E} [press ${R}N/n ${E}] ? " -n 1 -r
+echo -e "Do you want to install the 3D stuff ${G}now${E} [press ${G}Y${E}] or ${R}later${E} [press ${R}N/n ${E}] ? "
+read -p "" -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    sed -n '227,$p' inst.sh > /~/3d.sh
-    chmod +x /~/3d.sh
+    sed -n '228,$p' inst.sh > /home/$username/3d.sh
+    chmod +x /home/$username/3d.sh
     echo -e "Copied the ${G}3D stuff${E} to ${P}/~/3d.sh ${E}"  
 else    
 	while true; do
 	    read -p "Do you want to install the 3D stuff later? Y/N" yn
 	    case $yn in
-		[Yy]* ) sed -n '227,$p' inst.sh > /~/3d.sh
-			chmod +x /~/3d.sh; break;;
+		[Yy]* ) sed -n '228,$p' inst.sh > /home/$username/3d.sh
+			chmod +x /home/$username/3d.sh; break;;
 		[Nn]* ) read -p "Do you want to install wmware guest system tools? " -n 1 -r
 								echo    # (optional) move to a new line
 									if [[ $REPLY =~ ^[Yy]$ ]]
