cb16377336324a092896a8131308439cd93dedd7
ghartausz <ghartausz@gmail.com>
2022-12-29 17:50:22 +0200

    Update fstr.sh

--- fstr.sh
+++ fstr.sh
@@ -19,15 +19,14 @@ echo
 echo "Enter the device you want to install to (e.g. /dev/sda):"
 read device
 parted -s "$device" mklabel gpt
-parted -s "$device" mkpart "EFI system partition" fat32 1MiB 512MiB
+parted -s "$device" mkpart primary "EFI system partition" fat32 1MiB 512MiB
 parted -s "$device" set 1 esp on
 mkfs.fat -F32 "${device}1"
-parted -s "$device" mkpart "root partition" ext4 512MiB 100%
+parted -s "$device" mkpart primary  "root partition" ext4 512MiB 100%
 parted -s "$device" set 2 lvm on
 mkfs.ext4 "${device}2"
 mount "${device}2" /mnt
 mount --mkdir ${device}1" /mnt/boot
-mkdir /mnt/efi
 echo "Server = http://mirrors.kernel.org/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 reflector --country Romania, --protocol https --latest 5 --save /etc/pacman.d/mirrorlist
 sudo pacman -S archlinux-keyring
